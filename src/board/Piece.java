46ca1cc62452d5e21222a465d49f2efe822f8e22
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-05-18 19:29:19 -0300

    creating packages and board classes

---
+++ src/board/Piece.java
@@ -0,0 +1,19 @@
+package board;
+
+public class Piece {
+    private Board board;
+    protected Position position;
+
+    public Piece(Board board, Position position) {
+        this.board = board;
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+}
